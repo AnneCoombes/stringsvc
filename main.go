9bf085d5f387c02af1ad755de3913db4f0729b4f
Anne Coombes <anne@besabeau.com>
2020-12-13 15:49:59 +0000

    added logger

--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 func main() {
-    svc := stringService{}
+    logger := log.NewLogfmtLogger(os.Stderr)
+
+    var svc StringService
+    svc = stringServcie{}
+    svc = loggingMiddleware(logger, svc)
+
+    var uppercase endpoint.Endpoint
+
+    uppercase = makeUppercaseEndpoint(svc)
+    uppercase = loggingMiddleware(log.With(logger, "method", "uppercase")) (uppercase)
+
+    var count endpoint.Endpoint
+    count = makeCountEndpoint(svc)
+    count = loggingMiddleware(log.With(logger, "method", "count")) (count)
+
     uppercaseHandler := httptransport.NewServer(
-        makeUppercaseEndpoint(svc),
+        uppercase,
         decodeUppercaseRequest,
         encodeResponse,
     )
 
     countHandler := httptransport.NewServer(
-        makeCountEndpoint(svc), 
+        count, 
         decodeCountRequest, 
         encodeResponse, 
     )
 
     http.Handle("/uppercase", uppercaseHandler)
     http.Handle("/count", countHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
